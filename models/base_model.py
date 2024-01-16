ff07f5894fe4c7e86418bfec1e57cacf32be6c05
Khayaeljuniormokoena <khayaeljuniormokoena>
2024-01-16 13:06:49 -0800

    update

--- models/base_model.py
+++ models/base_model.py
@@ -10,13 +10,28 @@ class BaseModel:
     """
     BaseModel class that defines common attributes/methods for other classes.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of BaseModel.
+
+        Args:
+            kwargs (dict): Dictionary representation of the instance.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
